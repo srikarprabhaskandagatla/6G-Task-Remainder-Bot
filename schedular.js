82ee749fe05cee4f87b91ad6c03d9dfdbe19cdce
srikarprabhaskandagatla <skandagatla@umass.edu>
2025-05-30 15:53:24 -0500

    .gitignore Configuration

--- schedular.js
+++ schedular.js
@@ -3,3 +3,6 @@ const fs = require('fs');
 const twilio = require('twilio');
 const express = require('express');
 const bodyParser = require('body-parser');
+require('dotenv').config();
+
+const client = twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
