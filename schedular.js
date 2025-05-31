81988e70071ea4582f40882813aa7baa9e199bb9
srikarprabhaskandagatla <skandagatla@umass.edu>
2025-05-31 12:06:27 -0500

    Add state.json and load, save state functionality in schedular

--- schedular.js
+++ schedular.js
@@ -1,3 +1,4 @@
+// Import necessary modules
 const cron = require('node-cron');
 const fs = require('fs');
 const twilio = require('twilio');
@@ -7,8 +8,30 @@ require('dotenv').config();
 
 const client = twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
 
+// Roommates for names and phone numbers
 const people = [
   {name: NAME_1, phone: PHONE_NUMBER_1},
   {name: NAME_2, phone: PHONE_NUMBER_2},
   {name: NAME_3, phone: PHONE_NUMBER_3}
 ];
+
+// To schedule tasks, the state is loaded from state.json
+function loadState() {
+  return JSON.parse(fs.readFileSync('state.json'));
+}
+
+// Save the current state to state.json
+function saveState(state) {
+  fs.writeFileSync('state.json', JSON.stringify(state, null, 2));
+}
+
+function sendCustomMessage(person, message) {
+  client.messages
+    .create({
+      body: message,
+      from: process.env.TWILIO_WHATSAPP_NUMBER,
+      to: person.phone
+    })
+    .then(msg => console.log(`Sent reminder to ${person.name}: ${msg.sid}`))
+    .catch(err => console.error(err));
+}
