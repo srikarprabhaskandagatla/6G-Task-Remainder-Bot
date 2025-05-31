0d9d03d9790073d1f731f206ee18d2ef3a6516d1
srikarprabhaskandagatla <skandagatla@umass.edu>
2025-05-31 11:57:10 -0500

    Add Names Variables to environment variables and schedular

--- schedular.js
+++ schedular.js
@@ -6,3 +6,9 @@ const bodyParser = require('body-parser');
 require('dotenv').config();
 
 const client = twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
+
+const people = [
+  {name: NAME_1, phone: PHONE_NUMBER_1},
+  {name: NAME_2, phone: PHONE_NUMBER_2},
+  {name: NAME_3, phone: PHONE_NUMBER_3}
+];
